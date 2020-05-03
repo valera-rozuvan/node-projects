30a2f029901fffb090be2ff783c4c68ed7d8a8bb
Valera Rozuvan <valera.rozuvan@gmail.com>
2020-05-03 20:24:51 +0300

    Format '05/main.js' file.

--- 05/main.js
+++ 05/main.js
@@ -1,5 +1,5 @@
 var LBL = require('./line-by-line.js'),
-    lbl;
+  lbl;
 
 console.log('[main]: calling LBL() function.');
 lbl = LBL('pg9603.txt', onEnd);
@@ -11,12 +11,12 @@ lbl(processLine);
 return;
 
 function processLine(lines, i) {
-    console.log('[processLine]: lines = ', lines);
-    console.log('[processLine]: i = ', i);
+  console.log('[processLine]: lines = ', lines);
+  console.log('[processLine]: i = ', i);
 }
 
 function onEnd(data, i) {
-    console.log('[onEnd]: data = ', data);
-    console.log('[onEnd]: i = ', i);
-    console.log();
+  console.log('[onEnd]: data = ', data);
+  console.log('[onEnd]: i = ', i);
+  console.log();
 }
