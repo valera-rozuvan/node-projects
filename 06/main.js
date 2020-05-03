bab887bd3d2332306837ba19c47d0c7bc7e00311
Valera Rozuvan <valera.rozuvan@gmail.com>
2020-05-03 20:25:39 +0300

    Format '06/main.js' file.

--- 06/main.js
+++ 06/main.js
@@ -6,25 +6,25 @@
  *     - http://stackoverflow.com/questions/13087888/getting-the-page-title-from-a-scraped-webpage
  */
 
-(function (undefined) {
-    'use strict';
+(function(undefined) {
+  'use strict';
 
-    var exec = require('child_process').exec,
-        url = 'http://www.youtube.com/watch?v=Hh4CdrT-hxY&list=RDAf_xcUvZj3k',
-        grepRegEx = /<title>(.*?)<\/title>/i,
-        child;
+  var exec = require('child_process').exec,
+    url = 'http://www.youtube.com/watch?v=Hh4CdrT-hxY&list=RDAf_xcUvZj3k',
+    grepRegEx = /<title>(.*?)<\/title>/i,
+    child;
 
-    console.log('Getting title of page "' + url + '".');
+  console.log('Getting title of page "' + url + '".');
 
-    child = exec(
-        'wget -q -O - "$@" ' + url,
-        function (error, stdout, stderr) {
-            if (error !== null) {
-                console.log('Error: ' + error);
-                console.log('stderr: ' + stderr);
-            } else {
-                console.log('Title: "' + stdout.match(grepRegEx)[1] + '".');
-            }
-        }
-    );
+  child = exec(
+    'wget -q -O - "$@" ' + url,
+    function(error, stdout, stderr) {
+      if (error !== null) {
+        console.log('Error: ' + error);
+        console.log('stderr: ' + stderr);
+      } else {
+        console.log('Title: "' + stdout.match(grepRegEx)[1] + '".');
+      }
+    }
+  );
 }).call(this);
